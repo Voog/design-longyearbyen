bc9dea7ceb8972ee4f7d5055ac7758bb2fac85ca
Sirje Siilik <sirje.siilik@gmail.com>
2017-01-09 11:33:03 +0200

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit ac6c49736fb5148633eadf91be385c9081a5691a.

--- sources/javascripts/concat/base.js
+++ sources/javascripts/concat/base.js
@@ -404,7 +404,7 @@
   // ===========================================================================
   // Binds editmode image drop areas.
   // ===========================================================================
-  var bindItemImgDropAreas = function() {
+  var bindItemImgDropAreas = function(placeholderText) {
     $('.js-img-drop-area').each(function(index, imgDropAreaTarget) {
       var $imgDropAreaTarget = $(imgDropAreaTarget),
           $contentItemBox = $imgDropAreaTarget.closest('.js-content-item-box'),
@@ -424,6 +424,7 @@
         positionable: false,
         target_width: 1280,
         removeBtn: '',
+        placeholder: '<div class="edy-img-drop-area-placeholder">' + placeholderText + '</div>',
 
         change: function(data) {
           var $bgPickerButton = $contentItemBox.find('.js-bg-picker-btn');
